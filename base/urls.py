b249d7af723eefe9edb56e04612df88ec3609c49
wiktor bozek <wiktorsebbozek@gmail.com>
2023-03-14 17:00:12 +0100

    Add urls for new pages

--- base/urls.py
+++ base/urls.py
@@ -3,7 +3,9 @@ from . import views
 
 urlpatterns= [
     path('', views.home, name="home"),
-
+    path('contact/', views.contact, name="contact"),
+    path('email-verification/', views.email_verification, name="email-verification"),
+    path('thank-you/', views.thankyou_page, name="thank-you"),
 
 ]
 
