878e0fe508d39bd2758db5f1c95c2d7471c80621
wiktor bozek <wiktorsebbozek@gmail.com>
2023-03-14 17:03:46 +0100

    Add functions inside views
    basic functions to render templates

--- base/views.py
+++ base/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render, redirect
 
 
-# Create your views here.
-
 def home(request):
     context = {}
     return render(request, "base/home.html", context)
+
+def contact(request):
+    context = {}
+    return render(request, 'base/contact-page.html', context)
+
+def email_verification(request):
+    context = {}
+    return render(request, 'base/email-verification.html', context)
+
+def thankyou_page(request):
+    context = {}
+    return render(request, 'base/thankyou-page.html', context)
