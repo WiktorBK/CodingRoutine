3344e3e699e5d274f36f6bbe967accb22f343777
wiktor bozek <wiktorsebbozek@gmail.com>
2023-03-16 12:57:25 +0100

    Create model for contact message

--- base/models.py
+++ base/models.py
@@ -5,7 +5,17 @@ class Newsletter_User(models.Model):
     verified = models.BooleanField(default=False)
     created = models.DateTimeField(auto_now_add=True)
 
-    class Meta:
-        ordering = ['-created']
-    def __str__(self):
-        return self.email
+    class Meta: ordering = ['-created']
+    def __str__(self): return self.email
+    
+
+class Message_contact(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    sent = models.DateTimeField(auto_now_add=True)
+    message = models.TextField()
+
+
+    class Meta: ordering = ['-sent']
+    def __str__(self): return self.message
