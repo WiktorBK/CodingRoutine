5b8219102b059d9c2b18ab91f948df3b77b95ee3
wiktor bozek <wiktorsebbozek@gmail.com>
2023-03-16 14:14:07 +0100

    Apply styling to django forms

--- base/forms.py
+++ base/forms.py
@@ -1,10 +1,10 @@
 from django.forms import ModelForm
+from django import forms
 from .models import Newsletter_User, Message_contact
 
-class NewsletterUserForm(ModelForm):
-    class Meta:
-        model = Newsletter_User
-        fields = ['email']
+class NewsletterUserForm(forms.Form):
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder' :'Enter your email', 'id': 'email', 'type':"text"}))
+
 
 
 class MessageContactForm(ModelForm):
