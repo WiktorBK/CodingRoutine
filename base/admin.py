aa1135a91543d5fc0c21a9f6d9b5c04c7d8a2fd3
wiktor bozek <wiktorsebbozek@gmail.com>
2023-03-16 19:59:56 +0100

    Register Message object on admin site

--- base/admin.py
+++ base/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Newsletter_User
+from .models import Newsletter_User, Message_contact
 
 admin.site.register(Newsletter_User)
+admin.site.register(Message_contact)
+
 
