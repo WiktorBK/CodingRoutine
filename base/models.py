23e68c92f2657ba02362b0f9cd9f44d2fda2830c
wiktor bozek <wiktorsebbozek@gmail.com>
2023-03-16 12:44:30 +0100

    Create newsletter user db model

--- base/models.py
+++ base/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Newsletter_Users(models.Model):
+    email = models.CharField(max_length=200)
+    verified = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created']
+    def __str__(self):
+        return self.email
