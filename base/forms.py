a7188ea0eecf49fe52e236934bb5e713c22aea84
wiktor bozek <wiktorsebbozek@gmail.com>
2023-03-16 13:01:06 +0100

    Create form for contact page

--- base/forms.py
+++ base/forms.py
@@ -1,7 +1,13 @@
 from django.forms import ModelForm
-from .models import Newsletter_User
+from .models import Newsletter_User, Message_contact
 
 class NewsletterUserForm(ModelForm):
     class Meta:
         model = Newsletter_User
         fields = ['email']
+
+
+class MessageContactForm(ModelForm):
+    class Meta:
+        model = Message_contact
+        fields = '__all__'
