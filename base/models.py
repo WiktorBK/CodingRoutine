e07ffbb5f34fff688a0680d28f7ff56b8d7dbdc4
wiktor bozek <wiktorsebbozek@gmail.com>
2023-03-20 18:06:36 +0100

    Change the function to only generate email

--- base/models.py
+++ base/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 from django.core.mail import EmailMessage
 from django.conf import settings
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.encoding import force_bytes
+from .tokens import email_verification_token
 
 class Newsletter_User(models.Model):
 
@@ -8,14 +13,21 @@ class Newsletter_User(models.Model):
     verified = models.BooleanField(default=False)
     created = models.DateTimeField(auto_now_add=True)
 
-    def verify_email(self, to_email, first_name):
-        email = EmailMessage(
-            f"Verify your email",
-            f"Hello, {first_name}! Thank you for subscribing to codingRoutine!",
-            settings.DEFAULT_FROM_EMAIL,
-            [to_email],
-        )
-        email.send()
+    def generate_verification_email(self, request, user, to_email):
+        mail_subject = "Verify your email address"
+        message = render_to_string('base/template_email.html', 
+            {
+            'domain': get_current_site(request).domain,
+            'uid':  urlsafe_base64_encode(force_bytes(user.id)),
+            'token': email_verification_token.make_token(user),
+            'protocol': 'https' if request.is_secure() else 'http'
+            })
+        email = EmailMessage(mail_subject, message, to = [to_email])
+
+        return email
+            
+        
+
 
     class Meta: ordering = ['-created']
     def __str__(self): return self.email
