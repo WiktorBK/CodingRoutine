20b943b9db8614ad0a7c5eb88c1b99e7432079a3
wiktor bozek <wiktorsebbozek@gmail.com>
2023-03-25 20:30:50 +0100

    Register models to admin view

--- base/admin.py
+++ base/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Newsletter_User, Message_contact, CodingExcercise
+from .models import Newsletter_User, Message_contact, CodingExcercise, ExceptionTracker
 
 admin.site.register(Newsletter_User)
 admin.site.register(Message_contact)
 admin.site.register(CodingExcercise)
+admin.site.register(ExceptionTracker)
+
 
