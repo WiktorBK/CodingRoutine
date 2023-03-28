17c6c4d0983f9203aa520ef5d9b900064104f00d
wiktor bozek <wiktorsebbozek@gmail.com>
2023-03-29 00:30:47 +0200

    Add unsubscribe token generator

--- base/tokens.py
+++ base/tokens.py
@@ -4,5 +4,12 @@ import six
 class VerifyEmailTokenGenerator(PasswordResetTokenGenerator):
     def _make_hash_value(self, user, timestamp):
         return six.text_type(user.pk) + six.text_type(timestamp) + six.text_type(user.verified) 
+
+class UnsubscribeTokenGenerator():
+    def _make_hash_value(self, user):
+        return six.text_type(user.id) + six.text_type(user.email) + six.text_type(user.active)
     
+
 email_verification_token = VerifyEmailTokenGenerator()
+unsubscribe_token = UnsubscribeTokenGenerator()
+
