b99759caef1a0424896c0a4985efab0fcee4d786
wiktor bozek <wiktorsebbozek@gmail.com>
2023-03-28 14:58:14 +0200

    Import secret key

--- codingroutine/settings.py
+++ codingroutine/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 import secret_values
 
-SECRET_KEY = secret_values.security_key
+SECRET_KEY = secret_values.secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
