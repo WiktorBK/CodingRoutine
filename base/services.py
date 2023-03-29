91abbf030db1d36a25828c0bdca0cd1d7c002151
wiktor bozek <wiktorsebbozek@gmail.com>
2023-03-29 20:59:05 +0200

    Add create user function

--- base/services.py
+++ base/services.py
@@ -1,17 +1,19 @@
-from .models import Newsletter_User, ExceptionTracker
+from .models import Newsletter_User, ExceptionTracker, Message_contact
+from .tokens import unsubscribe_token
 
 from django.core.mail import EmailMessage
 from django.template.loader import render_to_string
 
 
-'''
-Sending coding excercise to each user individually according to excercise's order
 
-Email will be sent everyday at 8 am.
-
-'''
 
 def send_excercise():
+    '''
+    Sending coding excercise to each user individually according to excercise's order
+
+    Email will be sent everyday at 8 am.
+
+    '''
     users = Newsletter_User.objects.filter(verified=True)
     
     for user in users: 
@@ -29,3 +31,18 @@ def send_excercise():
             email.send()
         except Exception as e:
             ExceptionTracker.objects.create(title=f"Failed to send excercise email to {user.email}", exception=e)
+
+def create_user(email):
+        
+        '''
+        Create new user with given email and generated unsubscribe token.
+
+        Save to database
+        '''
+        new_user = Newsletter_User.objects.create(email = email)
+        new_user.unsubscribe_token = unsubscribe_token.generate_unsubscribe_token(new_user)
+        new_user.save()
+        return new_user
+        
+
+
