8b10ef3837b6c0877b90be5133e586daafe429be
wiktor bozek <wiktorsebbozek@gmail.com>
2023-03-29 19:23:18 +0200

    Modify unsubscribe token generator

--- base/tokens.py
+++ base/tokens.py
@@ -1,13 +1,16 @@
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
 import six
+import time
+import base64
 
 class VerifyEmailTokenGenerator(PasswordResetTokenGenerator):
     def _make_hash_value(self, user, timestamp):
         return six.text_type(user.pk) + six.text_type(timestamp) + six.text_type(user.verified) 
 
 class UnsubscribeTokenGenerator():
-    def _make_hash_value(self, user):
-        return six.text_type(user.id) + six.text_type(user.email) + six.text_type(user.active)
+    def generate_unsubscribe_token(self, user):
+        return base64.b64encode(str(user.id).encode("ascii") + user.email.encode("ascii") + str(time.time()).encode("ascii")
+                                 + str(user.verified).encode("ascii")).decode('ascii')
     
 
 email_verification_token = VerifyEmailTokenGenerator()
