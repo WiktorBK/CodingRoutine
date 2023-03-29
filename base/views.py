0d0f8f46391cbe9ddb5c61d406aedc73753e8d79
wiktor bozek <wiktorsebbozek@gmail.com>
2023-03-29 21:00:11 +0200

    Simplify views functions

--- base/views.py
+++ base/views.py
@@ -10,52 +10,6 @@ from .models import Newsletter_User, Message_contact, ExceptionTracker, CodingEx
 from .tokens import email_verification_token, unsubscribe_token
 from .services import *
 
-def home(request):
-    form = NewsletterUserForm()
-    context = {'form': form}
-    if request.method == "POST":
-
-        form = NewsletterUserForm(request.POST)
-        email = request.POST.get('email').lower()  
-
-        # try:
-        validate_email(email)
-        user = Newsletter_User.objects.filter(email=email).first()
-        if user:
-            verified = user.verified
-            context = {'form': form, 'enrolled': True, 'verified': verified, 'email': email, 'active': user.active}
-        else:
-            new_user = Newsletter_User.objects.create(email = email)
-            new_user.unsubscribe_token = unsubscribe_token.generate_unsubscribe_token(new_user)
-            new_user.save()
-            verification_email = new_user.generate_verification_email(request)
-            return HttpResponseRedirect('email-verification') if verification_email.send() else messages.error("something went wrong")
-        # except: 
-        #     messages.error(request, "Enter valid email address")
-
-    return render(request, "base/home.html", context=context)
-
-
-
-def contact(request):
-    form = MessageContactForm()
-    if request.method == "POST":
-        form = MessageContactForm(request.POST)
-        email = request.POST.get('email_contact').lower()
-        first_name = request.POST.get('first_name').capitalize()
-        last_name = request.POST.get('last_name').capitalize()
-        message = request.POST.get('message')
-        
-        Message_contact.objects.create(
-        email_contact=email, 
-        first_name=first_name, 
-        last_name=last_name, 
-        message=message)
-        context = {'form': form, 'first_name': first_name}
-        return render(request, "base/message_sent.html", context=context)
-    
-    context = {'form': form}
-    return render(request, 'base/contact-page.html', context=context)
 
 def email_verification(request):
     context = {}
@@ -93,21 +47,30 @@ def unsubscribe(request, uidb64, token):
     try:
         uid = int(urlsafe_base64_decode(uidb64))
         user = Newsletter_User.objects.get(id=uid)
-    except: user = None
-
-
-    #  needs change
-    # if user and email_verification_token.check_token(user, token): 
-    #     user.active = False
-    #     user.verified = False
-    #     user.save()
-        
+    except: return HttpResponse("Error: Activation link is invalid")
+    
+    if user and user.unsubscribe_token == token:
+        user.active = False
+        user.verified = False
+        user.save()
+    else: return HttpResponse("Error: Activation link is invalid")
 
     return render(request, "base/unsubscribe-page.html")
 
+def contact(request):
+    form = MessageContactForm()
+    if request.method == "POST":
+        form = MessageContactForm(request.POST)
+        message = create_message(request)
+
+        context = {'form': form, 'first_name': message.first_name, 'sent': True}
+        return render(request, "base/message_sent.html", context=context)
+    
+    context = {'form': form}
+    return render(request, 'base/contact-page.html', context=context)
+
 @user_passes_test(lambda u: u.is_superuser)
 def resend(request, email):
-
     try: 
         user = Newsletter_User.objects.get(email=email)
         email = user.generate_verification_email(request)
@@ -116,7 +79,28 @@ def resend(request, email):
     except Exception as e: 
         ExceptionTracker.objects.create(title="Failed to resend verification link", exception=e)
 
+def home(request):
+    form = NewsletterUserForm()
+    context = {'form': form}
+    if request.method == "POST":
 
+        form = NewsletterUserForm(request.POST)
+        email = request.POST.get('email').lower()  
+
+        try:
+            validate_email(email)
+            user = Newsletter_User.objects.filter(email=email).first()
+            if user:
+                verified = user.verified
+                context = {'form': form, 'enrolled': True, 'verified': verified, 'email': email, 'active': user.active}
+            else:
+                new_user = create_user(email)
+                verification_email = new_user.generate_verification_email(request)
+                return HttpResponseRedirect('email-verification') if verification_email.send() else messages.error("something went wrong")
+        except: 
+            messages.error(request, "Enter valid email address")
+
+    return render(request, "base/home.html", context=context)
 
 def page_not_found(request, exception, template_name='404.html'):
     
