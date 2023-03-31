dabfa0f86d4a7ee9861d931593780aa36aaa22a4
wiktor bozek <wiktorsebbozek@gmail.com>
2023-03-31 22:42:11 +0200

    Include urls from administration app

--- codingroutine/urls.py
+++ codingroutine/urls.py
@@ -4,7 +4,9 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('base.urls'))
+    path('', include('base.urls')),
+    path('', include('administration.urls'))
+
 ]
 
 handler404 = 'base.views.page_not_found'
