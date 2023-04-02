1dcfaeac3db33bbbfc04020877ef3d7e6de2b3e7
wiktor bozek <wiktorsebbozek@gmail.com>
2023-04-02 21:23:09 +0200

    Admin: add urls for administration app

--- administration/urls.py
+++ administration/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns= [
-    path("administration/", views.administration_site, name="administration-site")
+    path("administration/", views.administration_site, name="administration-site"),
+    path("administration/users", views.users, name="users"),
+    path("administration/messages", views.messages, name="messages"),
+    path("administration/exceptions", views.exceptions, name="exceptions"),
+    path("administration/excercises", views.excercises, name="excercises"),
+
+
 ]
