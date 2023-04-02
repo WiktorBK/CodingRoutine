ec8e69c0034e0bb949c05b188c9d63f707c76209
wiktor bozek <wiktorsebbozek@gmail.com>
2023-04-02 21:23:30 +0200

    Admin: add views for administration app

--- administration/views.py
+++ administration/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import user_passes_test
+from .models import *
 
 
 @user_passes_test(lambda u: u.is_superuser)
 def administration_site(request):
+    return render(request, 'administration/administration.html')
+
+@user_passes_test(lambda u: u.is_superuser)
+def users(request):
+    return render(request, 'administration/administration.html')
+
+@user_passes_test(lambda u: u.is_superuser)
+def exceptions(request):
+    return render(request, 'administration/administration.html')
+
+@user_passes_test(lambda u: u.is_superuser)
+def messages(request):
+    return render(request, 'administration/administration.html')
+
+@user_passes_test(lambda u: u.is_superuser)
+def excercises(request):
     return render(request, 'administration/administration.html')
