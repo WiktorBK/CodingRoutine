b8127fe6460cc761775dd6303caf88b9d6952d2e
wiktor bozek <wiktorsebbozek@gmail.com>
2023-04-06 15:41:38 +0200

    Admin: add context dictionary to every view function

--- administration/views.py
+++ administration/views.py
@@ -1,24 +1,52 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.models import User
+
 from .models import *
+from base.models import *
 
 
 @user_passes_test(lambda u: u.is_superuser)
 def administration_site(request):
-    return render(request, 'administration/administration.html')
+    unread_messages = len(Message_contact.get_unread_messages())
+    unread_exceptions = len(ExceptionTracker.get_unread_exceptions())
+
+   
+
+    context={"unread_messages": unread_messages, "unread_exceptions": unread_exceptions}
+    return render(request, 'administration/administration.html', context=context)
 
 @user_passes_test(lambda u: u.is_superuser)
 def users(request):
-    return render(request, 'administration/administration.html')
+    users_ = Newsletter_User.get_users()
+
+    context={"users": users_}
+    return render(request, 'administration/users.html', context=context)
 
 @user_passes_test(lambda u: u.is_superuser)
 def exceptions(request):
-    return render(request, 'administration/administration.html')
+    exceptions_ = ExceptionTracker.get_exceptions()
+
+    context={"exceptions": exceptions_}
+    return render(request, 'administration/exceptions.html', context=context)
 
 @user_passes_test(lambda u: u.is_superuser)
 def messages(request):
-    return render(request, 'administration/administration.html')
+    messages_=Message_contact.get_messages()
+
+    context={"messages": messages_}
+    return render(request, 'administration/messages.html', context=context)
 
 @user_passes_test(lambda u: u.is_superuser)
 def excercises(request):
-    return render(request, 'administration/administration.html')
+    excercises_ = CodingExcercise.get_excercises()
+
+    context={"excercises": excercises_}
+    return render(request, 'administration/excercises.html', context=context)
+
+@user_passes_test(lambda u: u.is_superuser)
+def admins(request):
+    admins_ = User.objects.filter(is_superuser=True)
+
+    context={"admins": admins_}
+    return render(request, 'administration/admins.html')
