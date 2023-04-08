38385bdc95205c19a6ffdf23585a4159ef8d2319
wiktor bozek <wiktorsebbozek@gmail.com>
2023-04-08 21:52:10 +0200

    Admin: update widgets to excercise forms

--- administration/forms.py
+++ administration/forms.py
@@ -8,12 +8,13 @@ class AddExcerciseForm(ModelForm):
         model=CodingExcercise
         fields = '__all__'
         widgets ={
-            'title': forms.TextInput(attrs={'id':'excercise-form', 'type': 'text', 'placeholder': 'Excercise title',}),
-            'body': forms.Textarea(attrs={'id':'excercise-form', 'type': 'text', 'placeholder': 'Excercise body',}),
-            'example_input': forms.TextInput(attrs={'id':'excercise-form', 'type': 'text', 'placeholder': 'e.g [2, 3, 4, 5]'}),
-            'example_output': forms.TextInput(attrs={'id':'excercise-form', 'type': 'text', 'placeholder': 'e.g [5, 4, 3, 2]'})
-
+            'difficulty': forms.TextInput(attrs={'id': 'difficulty-form', 'maxlength':'1000'}),
+            'title': forms.TextInput(attrs={'id':'title-form', 'type': 'text', 'placeholder': 'Excercise title',}),
+            'body': forms.Textarea(attrs={'id':'body-form', 'type': 'text', 'placeholder': 'Excercise body',}),
+            'example_input': forms.TextInput(attrs={'id':'example-form', 'type': 'text', 'placeholder': 'e.g [2, 3, 4, 5]'}),
+            'example_output': forms.TextInput(attrs={'id':'example-form', 'type': 'text', 'placeholder': 'e.g [5, 4, 3, 2]'})
         }
+
 
 
 
