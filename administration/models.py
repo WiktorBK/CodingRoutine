3e1ae65b19d0e9410f63fbb197c71755b6e10864
wiktor bozek <wiktorsebbozek@gmail.com>
2023-04-10 22:32:23 +0200

    Admin: relocate ExceptionTracker to administration app

--- administration/models.py
+++ administration/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class ExceptionTracker(models.Model):
+    occured = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100)
+    exception = models.CharField(max_length=300)
+    unread = models.BooleanField(default=True)
+
+    class Meta:ordering = ['-occured']
+    def __str__(self): return self.title
+
+    @classmethod
+    def get_exceptions(cls): return cls.objects.filter()
+
+    @classmethod
+    def get_unread_exceptions(cls): return cls.objects.filter(unread=True)
